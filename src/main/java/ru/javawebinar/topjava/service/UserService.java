b0009d514f44568d69bf05ea6f40007377bf5ddc
Evgen-Mutagen <jeka190@mail.ru>
2022-03-09 16:05:20 +0300

    HW5 - add optional 5,6

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -51,4 +51,7 @@ public class UserService {
         Assert.notNull(user, "user must not be null");
         checkNotFoundWithId(repository.save(user), user.id());
     }
+    public User getUserWithMeal(int id) {
+        return checkNotFoundWithId(repository.getUserWithMeals(id), id);
+    }
 }
