336ea0d23dc2dd55aaf1352b8921b74824204d90
Evgen-Mutagen <jeka190@mail.ru>
2022-03-28 20:21:59 +0300

    HW7 - done

---
+++ curl.md
@@ -0,0 +1,4 @@
+GET http://localhost:8080/topjava/rest/admin/users
+GET http://localhost:8080//topjava/rest/admin/users/100001
+DELETE http://localhost:8080/topjava/rest/admin/meals/100001
+GET http://localhost:8080/topjava/rest/profile/filter?startDate=2020-01-30&endDate=2020-01-31&startTime=01%3A00&endTime=15%3A00
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,7 +1,75 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 
-@Controller
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.web.meal.MealRestController.REST_URL;
+
+@RestController
+@RequestMapping(value = REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealRestController extends AbstractMealController {
+    static final String REST_URL = "/rest/meals";
+
+    @Override
+    @GetMapping("/{id}")
+    public Meal get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal) {
+        Meal newMeal = create(meal);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(REST_URL + "/{id}")
+                .buildAndExpand(newMeal.getId()).toUri();
+        return ResponseEntity.created(uri).body(newMeal);
+    }
+
+    @Override
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Meal meal, @PathVariable int id) {
+        super.update(meal, id);
+    }
+
+//    @GetMapping("/between")
+//    public List<MealTo> getBetween(
+//    @RequestParam @DateTimeFormat(
+//    iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+//    @RequestParam @DateTimeFormat(
+//    iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) {
+//        return super.getBetween(start.toLocalDate(), start.toLocalTime(), end.toLocalDate(), end.toLocalTime());
+//    }
+
+    @Override
+    @GetMapping("/between")
+    public List<MealTo> getBetween(@RequestParam LocalDate startDate,
+                                   @RequestParam LocalTime startTime,
+                                   @RequestParam LocalDate endDate,
+                                   @RequestParam LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
 }
